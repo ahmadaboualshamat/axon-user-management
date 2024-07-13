6ab0cacf13e1f5770732de194ba6cd7327f8c444
ahmad.aboalshamat <ahmad.aboalshamat@isoft.ae>
2024-07-13 22:02:25 +0400

    Add New Models

--- src/main/java/com/axon/usermanagement/UserManagementApplication.java
+++ src/main/java/com/axon/usermanagement/UserManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = {"com.commonlib", "com.axon.usermanagement"})
+@SpringBootApplication(scanBasePackages = {"com.commonlib","com.commonconfigurationlib",  "com.axon.usermanagement"})
 @EnableCaching
 @EnableDiscoveryClient
 public class UserManagementApplication {
--- src/main/resources/scripts/user-sql-1.sql
+++ src/main/resources/scripts/user-sql-1.sql
@@ -4,5 +4,5 @@ INSERT INTO SYS_USER(ID, username, password, status, roles,
                         CREATED_BY, CREATED_DATE,
                         MODIFIED_BY, MODIFIED_DATE)
 VALUES (nextval('user_seq'), 'ahmad',
-        '$2a$10$LFr.Lz/P688icEFLKK3YFOjkIHq6QT/oC/aBM50iFQHqmzaX6//.q', 'ACTIVE','["USER"]', 'SYSTEM', NOW(),
+        '$2a$10$UeiPAtcR9IJN4SFKiqiLC.cMYBXSNd03nAAHtNPuvQm2Pu.gfT.7C', 'ACTIVE','["USER"]', 'SYSTEM', NOW(),
         'SYSTEM', NOW() );
