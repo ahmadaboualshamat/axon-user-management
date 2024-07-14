c09e66ba3b97140af70b6fff1195e8ecf4e19290
ahmad.aboalshamat <ahmad.aboalshamat@isoft.ae>
2024-07-14 10:32:12 +0400

    Add New Models

--- src/main/resources/scripts/user-sql-1.sql
+++ src/main/resources/scripts/user-sql-1.sql
@@ -6,3 +6,11 @@ INSERT INTO SYS_USER(ID, username, password, status, roles,
 VALUES (nextval('user_seq'), 'ahmad',
         '$2a$10$UeiPAtcR9IJN4SFKiqiLC.cMYBXSNd03nAAHtNPuvQm2Pu.gfT.7C', 'ACTIVE','["USER"]', 'SYSTEM', NOW(),
         'SYSTEM', NOW() );
+
+
+INSERT INTO SYS_USER(ID, username, password, status, roles,
+                     CREATED_BY, CREATED_DATE,
+                     MODIFIED_BY, MODIFIED_DATE)
+VALUES (nextval('user_seq'), 'ahmadaboualshamat',
+        '$2a$10$UeiPAtcR9IJN4SFKiqiLC.cMYBXSNd03nAAHtNPuvQm2Pu.gfT.7C', 'ACTIVE','["USER"]', 'SYSTEM', NOW(),
+        'SYSTEM', NOW() );
